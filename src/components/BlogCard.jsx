c314027cc398a3116108b183fb0d2a9163feb754
rahyanakil <rahyanakil89@gmail.com>
2025-02-02 16:15:13 +0600

    updated with the styles

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -9,8 +9,8 @@ const BlogCard = ({ blog, onBookmark }) => {
   };
 
   return (
-    <div className=" items-center border-b">
-      <img src={blog.image} alt={blog.title} className="w-16 h-16 " />
+    <div className=" max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+      <img src={blog.image} alt={blog.title} className="h-48 w-full object-cover md:w-48 " />
       <div className="ml-4">
         <h2 className="text-xl font-semibold">{blog.title}</h2>
       </div>
--- src/components/BookmarkList.jsx
+++ src/components/BookmarkList.jsx
@@ -1,7 +1,7 @@
 const BookmarkList = ({ bookmarks }) => {
     return (
       <div className="w-1/4 p-4 border-l">
-        <h2 className="text-xl font-semibold">Bookmarks ({bookmarks.length})</h2>
+        <h2 className="text-xl font-semibold">Bookmarked Blogs: ({bookmarks.length})</h2>
         <ul>
           {bookmarks.map((blog) => (
             <li key={blog.id} className="mt-2">
